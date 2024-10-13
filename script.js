66587899d34e265b600fea5f55844a2a9d64515e
Satinder Sidhu <satty2k2@yahoo.com>
2024-10-13 19:29:41 +0530

    media queries to be added and switch to be customized

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 const DarkMode = document.querySelector('#mode')
 const Dashboard = document.querySelector('.dashboard')
-
-const Card = document.querySelectorAll('.card')
+const Cards = document.querySelectorAll('.card')
 const Status = document.querySelectorAll('.status')
 const Header = document.querySelector('header')
 
-console.log(Card, Status)
+
 
 DarkMode.addEventListener('change', ()=>{
     if(DarkMode.checked){
         document.body.classList.add('dark')
         Dashboard.classList.add('dark')
-        Array.from(Card).forEach((c)=>{
-            c.classList.add('dark')
+        Header.classList.add('dark')
+        Cards.forEach((card)=>{
+            card.classList.add('dark')
         })
-        Array.from(Status).forEach((status)=>{
+        Status.forEach((status)=>{
             status.classList.add('dark')
         })
-        Header.classList.add('dark')
+        
     }
     else{
         document.body.classList.remove('dark')
         Dashboard.classList.remove('dark')
-        Array.from(Card).forEach((c)=>{
-            c.classList.remove('dark')
+        Header.classList.remove('dark')
+        Cards.forEach((card)=>{
+            card.classList.remove('dark')
         })
-        Array.from(Status).forEach((status)=>{
+        Status.forEach((status)=>{
             status.classList.remove('dark')
         })
-        Header.classList.remove('dark')
+        
     }
 })
