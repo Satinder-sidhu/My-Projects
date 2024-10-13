19982673f28f710c991c4fdeae20ee87993e4699
Satinder Sidhu <satty2k2@yahoo.com>
2024-10-13 17:16:10 +0530

    dark mode implemented

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const DarkMode = document.querySelector('#mode')
+const Dashboard = document.querySelector('.dashboard')
+
+const Card = document.querySelectorAll('.card')
+const Status = document.querySelectorAll('.status')
+const Header = document.querySelector('header')
+
+console.log(Card, Status)
 
 DarkMode.addEventListener('change', ()=>{
-    console.log("yes")
+    if(DarkMode.checked){
+        document.body.classList.add('dark')
+        Dashboard.classList.add('dark')
+        Array.from(Card).forEach((c)=>{
+            c.classList.add('dark')
+        })
+        Array.from(Status).forEach((status)=>{
+            status.classList.add('dark')
+        })
+        Header.classList.add('dark')
+    }
+    else{
+        document.body.classList.remove('dark')
+        Dashboard.classList.remove('dark')
+        Array.from(Card).forEach((c)=>{
+            c.classList.remove('dark')
+        })
+        Array.from(Status).forEach((status)=>{
+            status.classList.remove('dark')
+        })
+        Header.classList.remove('dark')
+    }
 })
